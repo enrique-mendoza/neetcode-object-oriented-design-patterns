5f98555c79e7f00acaa1c97907bce53129c6fcf9
Enrique Mendoza <luis.mendoza@infotec.mx>
2025-03-18 17:36:27 -0600

    feat(builder): exercise

---
+++ src/main/java/neetcode/courses/designpatterns/builder/exercise/builders/MealBuilder.java
@@ -0,0 +1,36 @@
+package neetcode.courses.designpatterns.builder.exercise.builders;
+
+import neetcode.courses.designpatterns.builder.exercise.products.Meal;
+
+public class MealBuilder {
+
+    private Meal meal;
+
+    public MealBuilder() {
+        this.meal = new Meal();
+    }
+
+    public MealBuilder addCost(double cost) {
+        this.meal.setCost(cost);
+        return this;
+    }
+
+    public MealBuilder addTakeOut(boolean takeOut) {
+        this.meal.setTakeOut(takeOut);
+        return this;
+    }
+
+    public MealBuilder addMainCourse(String main) {
+        this.meal.setMain(main);
+        return this;
+    }
+
+    public MealBuilder addDrink(String drink) {
+        this.meal.setDrink(drink);
+        return this;
+    }
+
+    Meal build() {
+        return this.meal;
+    }
+}
---
+++ src/main/java/neetcode/courses/designpatterns/builder/exercise/products/Meal.java
@@ -0,0 +1,44 @@
+package neetcode.courses.designpatterns.builder.exercise.products;
+
+public class Meal {
+
+    private double cost;
+
+    private boolean takeOut;
+
+    private String main;
+
+    private String drink;
+
+    public double getCost() {
+        return this.cost;
+    }
+
+    public boolean getTakeOut() {
+        return this.takeOut;
+    }
+
+    public String getMain() {
+        return this.main;
+    }
+
+    public String getDrink() {
+        return this.drink;
+    }
+
+    public void setCost(double cost) {
+        this.cost = cost;
+    }
+
+    public void setTakeOut(boolean takeOut) {
+        this.takeOut = takeOut;
+    }
+
+    public void setMain(String main) {
+        this.main = main;
+    }
+
+    public void setDrink(String drink) {
+        this.drink = drink;
+    }
+}
